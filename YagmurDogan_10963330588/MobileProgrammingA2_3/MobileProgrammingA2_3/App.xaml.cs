c3c751a8f9f6216e9726b514f7055f7b334d49ac
yagmurdogan8 <85808317+yagmurdogan8@users.noreply.github.com>
2021-12-12 22:34:43 +0300

    Add files via upload

---
+++ YagmurDogan_10963330588/MobileProgrammingA2_3/MobileProgrammingA2_3/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileProgrammingA2_3
+{
+    public partial class App : Application
+    {
+        public static List<List<String>> list = new List<List<String>>();
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+            
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ YagmurDogan_10963330588/MobileProgrammingA2_3/MobileProgrammingA2_3/MainPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileProgrammingA2_3
+{
+    public partial class MainPage : ContentPage
+    {        
+        
+        int count1, count2 = 0;
+      
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            String UserString = userIDMain.Text;
+            String HESString = HESMain.Text;
+
+            Boolean isExist3 = false;
+
+            for(int i = 0; i < App.list.Count(); i++)
+            {
+                if (App.list[i][0].Equals(UserString) && App.list[i][1].Equals(HESString))
+                {
+                    isExist3 = true;
+                    Page2.userIDDet = App.list[i][0];
+                    Page2.HESDet = App.list[i][1];
+                    Page2.userNameDet = App.list[i][2];
+                    Page2.ticketID = App.list[i][3];
+
+                    await Navigation.PushAsync(new Page2(), true);
+                }             
+            }
+            if(isExist3 == false)
+            {
+                noTck.IsVisible = true;
+                System.Threading.Thread.Sleep(1000);
+                await Navigation.PushAsync(new Page1(), true);
+                noTck.IsVisible = false;
+            }
+           
+
+        }
+        private void EyeTapped(object sender, EventArgs e)
+        {
+
+            try
+            {
+                count1++;
+
+                if(count1 % 2 == 0)
+                {
+                    userIDMain.IsPassword = true;
+                }
+
+                else
+
+                    userIDMain.IsPassword = false;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        private void EyeTappedHES(object sender, EventArgs e)
+        {
+
+            try
+            {
+                count2++;
+
+                if(count2 % 2 == 0)
+                {
+                    HESMain.IsPassword = true;
+                }
+                else
+
+                    HESMain.IsPassword = false;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+        }
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
